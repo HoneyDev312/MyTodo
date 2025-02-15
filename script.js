632eac951e67e5bd689a270536d8d4f4e96efba6
HoneyDev312 <honeydev312@gmail.com>
2025-02-15 08:26:25 +0100

    set up header, form and theming

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("test script.js");
+const input = document.querySelector("input");
+const button = document.getElementById("form-button");
+
+function checkField() {
+  let inputFilled = true;
+
+  if (input.value.trim() === "") {
+    inputFilled = false;
+  }
+
+  button.disabled = !inputFilled;
+}
+
+// Vérifie au chargement
+checkField();
+
+// Vérifie à chaque changement
+input.addEventListener("input", checkField);
+
+document
+  .getElementById("task-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let newTask = document.getElementById("newTask").value;
+    console.log(newTask);
+    this.reset();
+    checkField();
+  });
